01971d7d5bf771d86460ea0a54e0705b3c541681
smenard <smenard@student.42lyon.fr>
2026-03-10 18:46:57 +0100

    fix(ex0, ex1): change file path

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -9,14 +9,14 @@ def main() -> None:
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
     print("Initializing new storage unit: new_discovery.txt")
 
-    discovery_file = open("new_discovery.txt", mode="w")
+    discovery_file = open("../new_discovery.txt", mode="w")
     discovery_file.write("[ENTRY 001] New quantum algorithm discovered\n")
     discovery_file.write("[ENTRY 002] Efficiency increased by 347%\n")
     discovery_file.write(
         "[ENTRY 003] Archived by Data Archivist trainee\n"
     )
 
-    discovery_file = open("new_discovery.txt", mode="r")
+    discovery_file = open("../new_discovery.txt", mode="r")
     print(discovery_file.read())
 
     print("Data inscription complete. Storage unit sealed.")
